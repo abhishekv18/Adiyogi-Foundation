ff84657e567b59d6c6a138126c30dff9eb0977a6
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-07-13 21:54:19 +0530

    add

--- client/src/DonationPop.jsx
+++ client/src/DonationPop.jsx
@@ -12,7 +12,7 @@ const DonationPopup = () => {
         setIsVisible(true);
         setIsAnimating(true);
         sessionStorage.setItem('donation-popup-shown', 'true');
-      }, 5000);
+      }, 4000);
       return () => clearTimeout(timer);
     }
   }, []);
