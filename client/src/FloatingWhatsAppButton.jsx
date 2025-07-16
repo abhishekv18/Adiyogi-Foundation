4df45028cace75389b20f3ab79a31e6fde0f1832
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-07-16 08:22:32 +0530

    add

--- client/src/FloatingWhatsAppButton.jsx
+++ client/src/FloatingWhatsAppButton.jsx
@@ -16,17 +16,29 @@ const WhatsAppDrawer = () => {
   if (!show) return null;
 
   return (
-    <a
-      href="https://wa.me/919175033022"
-      target="_blank"
-      rel="noopener noreferrer"
-      title="Chat on WhatsApp"
-      className="fixed z-50 p-3 rounded-full shadow-lg bg-green-500 text-white 
-  hover:bg-green-600 hover:scale-110 transition-transform duration-300
-  bottom-20 right-4 md:bottom-6 md:right-10"
-    >
-      <FaWhatsapp size={24} />
-    </a>
+  //   <a
+  //     href="https://wa.me/919175033022"
+  //     target="_blank"
+  //     rel="noopener noreferrer"
+  //     title="Chat on WhatsApp"
+  //     className="fixed z-50 p-3 rounded-full shadow-lg bg-green-500 text-white 
+  // hover:bg-green-600 hover:scale-110 transition-transform duration-300
+  // bottom-20 right-4 md:bottom-6 md:right-10"
+  //   >
+  //     <FaWhatsapp size={24} />
+  //   </a>
+  <a
+  href="https://wa.me/919175033022?text=Namaste%20%F0%9F%99%8F%2C%20I%20visited%20your%20website%20and%20would%20like%20to%20know%20more%20about%20your%20temple%20services."
+  target="_blank"
+  rel="noopener noreferrer"
+  title="Chat on WhatsApp"
+  className="fixed z-50 p-3 rounded-full shadow-lg bg-green-500 text-white 
+hover:bg-green-600 hover:scale-110 transition-transform duration-300
+bottom-20 right-4 md:bottom-6 md:right-10"
+>
+  <FaWhatsapp size={24} />
+</a>
+
   );
 };
 
