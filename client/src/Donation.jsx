0d3e2b5ab5dc6a33064c8c1b0a4a2efec2cd7b5a
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-07-22 18:08:25 +0530

    add

--- client/src/Donation.jsx
+++ client/src/Donation.jsx
@@ -745,7 +745,7 @@ const navigate=useNavigate();
                 <div className="w-32 h-32 sm:w-40 sm:h-40 md:w-48 md:h-48 mx-auto bg-white rounded-lg flex items-center justify-center">
                   <img
                   src='/volunteers/WhatsApp Image 2025-07-05 at 8.36.53 PM (1).jpeg'
-                  className="w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32" style={{ color: '#C41E3A' }} 
+                  className="w-24 h-24 sm:w-28 sm:h-28 md:w-36 md:h-36" style={{ color: '#C41E3A' }} 
                   />
                  
                 </div>
