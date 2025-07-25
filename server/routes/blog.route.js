52e9ed13323222e9af37208eb16a313c002b7bf2
abhishekv18 <166051197+abhishekv18@users.noreply.github.com>
2025-07-25 10:45:20 +0530

    add

--- server/routes/blog.route.js
+++ server/routes/blog.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { addBlog, deleteBlogs, editBlog, getBlogs, handleImageUpload } from "../controllers/blog.controller.js";
 import { upload } from "../helpers/cloudinary.js";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+// import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
 
@@ -13,10 +13,15 @@ router
   .post(upload.single("my_file"), handleImageUpload); // "my_file" must match exactly
  
 
-router.route("/add").post(isAuthenticated,addBlog);
-router.route("/get").get(getBlogs);
-router.route("/update/:id").put(isAuthenticated,editBlog);
-router.route("/delete/:id").delete(isAuthenticated,deleteBlogs);
+// router.route("/add").post(isAuthenticated,addBlog);
+// router.route("/get").get(getBlogs);
+// router.route("/update/:id").put(isAuthenticated,editBlog);
+// router.route("/delete/:id").delete(isAuthenticated,deleteBlogs);
 
+
+router.route("/add").post(addBlog);
+router.route("/get").get(getBlogs);
+router.route("/update/:id").put(editBlog);
+router.route("/delete/:id").delete(deleteBlogs);
 
 export default router;
